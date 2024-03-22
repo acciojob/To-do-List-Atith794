c84e76e4e36fee490f1d418bebf3866a46f17f72
Atith794 <68183452+Atith794@users.noreply.github.com>
2024-03-22 21:05:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here
 
+let btn = document.getElementById("addTodoBtn");
+let ol = document.getElementById("todoList");
+
+function handleClick() {
+	let text = document.getElementById("newTodoInput").value;
+	if(text === ""){
+		return;
+	}
+	let li = document.createElement("li");
+	li.textContent = text;
+	ol.append(li);
+}
+
+
+
